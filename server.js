2c64b5b22853a1ff319e19f15ec5dc8b079795a7
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2025-01-31 17:14:14 +0100

    Modified

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.use(cors()); // Enable CORS
 
 app.get("/", (req, res) => {
   const data = {
-    email: process.env.EMAIL,
+    email: "shinar930@gmail.com",
     current_time: new Date().toISOString(),
-    github_url: process.env.URL,
+    github_url: "https://github.com/Solayiwo/hng_public_info_api.git",
   };
 
   res.status(200).json(data);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running at port http://localhost:${process.env.PORT}`);
+app.listen(4500, () => {
+  console.log(`Server is running at port http://localhost:${4500}`);
 });
